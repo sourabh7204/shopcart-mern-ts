aa1dd238d8c6e45f9114947025961e9602ae9426
sourabh7204 <sourabh722004@gmail.com>
2025-09-03 21:42:32 +0530

    Created Fotter Panel

--- constants/data.ts
+++ constants/data.ts
@@ -1,7 +1,29 @@
+//Data for NavBar
 export const headerData = [
   { title: "Home", href: "/" },
   { title: "Shop", href: "/shop" },
   { title: "Blog", href: "/blog" },
   { title: "Hot Deal", href: "/deal" },
   //   { title: "Contact", href: "/contact" },
+];
+
+//Data for Fotter
+export const quickLinksData = [
+  { title: "About us", href: "/about" },
+  { title: "Contact us", href: "/contact" },
+  { title: "Terms & Conditions", href: "/terms" },
+  { title: "Privacy Policy", href: "/privacy" },
+  { title: "FAQs", href: "/faqs" },
+  { title: "Help", href: "/help" },
+];
+
+//Data for
+export const categoriesData = [
+  { title: "Mobiles", href: "mobiles" },
+  { title: "Appliances", href: "appliances" },
+  { title: "Smartphones", href: "smartphones" },
+  { title: "Air Conditioners", href: "air-conditioners" },
+  { title: "Washing Machine", href: "washing-machine" },
+  { title: "Kitchen Appliances", href: "kitchen-appliances" },
+  { title: "gadget accessories", href: "gadget-accessories" },
 ];
